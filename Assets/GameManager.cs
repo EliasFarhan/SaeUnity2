5e885d9c5ee62684bf35e671bdfbde88e5ad5aa9
efarhan <elias.farhan@gmail.com>
2016-12-08 10:50:06 +0100

    First commit, adding GameManager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	[SerializeField] int prout = 3;
+
+	void Awake() {
+		prout = 6;
+	}
+
+	// Use this for initialization
+	void Start () {
+		//prout = 7;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		
+	}
+}
