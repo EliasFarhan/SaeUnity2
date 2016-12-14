a744172e4052dfd520b8dd85607929e82bdd7d59
Elias Farhan <elias.farhan@gmail.com>
2016-12-14 12:34:15 +0100

    A bit of prefabs and transform

--- Assets/Scripts/Black.cs
+++ Assets/Scripts/Black.cs
@@ -4,14 +4,36 @@ using System.Collections;
 public class Black : MonoBehaviour {
 
 	SpriteRenderer m_SpriteRenderer;
+
+	[SerializeField] Stream m_StreamPrefab;
+
 	// Use this for initialization
 	void Start () {
 		m_SpriteRenderer = GetComponent<SpriteRenderer> ();
 		m_SpriteRenderer.sortingOrder = 2;
+
+		for(int i = 0; i<50;i++)
+			InstantiateStream ();
+
 	}
-	
+
+
+	void InstantiateStream()
+	{
+		Stream newStream = Instantiate (m_StreamPrefab);
+		newStream.gameObject.name = "Prout";
+		newStream.a = 4;
+
+		float angleDegree = 45.0f;
+		float angleRadian = angleDegree / 180.0f * Mathf.PI;
+		angleDegree = angleRadian / Mathf.PI * 180.0f;
+		Mathf.Cos (angleRadian);
+	}
+
+
+
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
---
+++ Assets/Scripts/Stream.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stream : MonoBehaviour {
+
+	public int a = 3;
+
+	// Use this for initialization
+	void Start () {
+		BullshitChildren ();
+	}
+	void BullshitChildren()
+	{
+		transform.position = new Vector3 ();
+
+		transform.eulerAngles = new Vector3 (0, 0, 30.0f);
+
+		Vector3 prout = new Vector3 (
+			Mathf.Cos(transform.eulerAngles.z),
+			Mathf.Sin(transform.eulerAngles.z),
+			0.0f);
+		Debug.Log (gameObject.transform.GetChild(0).gameObject.name);
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+}
